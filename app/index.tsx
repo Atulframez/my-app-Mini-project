fd3243c15fd853aa39f803c5d3720531f352daf1
Atul Anand <atulanand092004@gmail.com>
2026-05-30 13:20:50 +0530

    Add-and-Updata

--- app/index.tsx
+++ app/index.tsx
@@ -1,21 +1,12 @@
 import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import { Text, View  } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        backgroundColor: "red",
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text style={{ fontSize: 20, fontWeight: "bold", color: "white", textAlign: "center", 
-        fontStyle: "italic" }}>
-        Welcome to the Atul-First-App
+    <View>
+      <Text style={{ fontSize: 20, fontWeight: "bold", color: "red", textAlign: "center", fontStyle: "bold" }}>
+        Welcome to the Mini-Project
       </Text>
-      <Link href={"/about"}>Go to about</Link>
     </View>
   );
 }
