1a9fc42d520d1148594065703430bcbc910c7679
Atul Anand <atulanand092004@gmail.com>
2026-05-31 11:58:45 +0530

    Add- login-page and signup-page

--- app/index.tsx
+++ app/index.tsx
@@ -4,7 +4,7 @@ import { Text, View  } from "react-native";
 export default function Index() {
   return (
     <View>
-      <Text style={{ fontSize: 20, fontWeight: "bold", color: "red", textAlign: "center", fontStyle: "bold" }}>
+      <Text>
         Welcome to the Mini-Project
       </Text>
     </View>
