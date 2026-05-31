1b0d17c9b927298169491bd154fc754aa9870d4b
Atul Anand <atulanand092004@gmail.com>
2026-05-31 13:07:12 +0530

    Add-Register-Page

--- app/signup.tsx
+++ app/signup.tsx
@@ -1,11 +1,19 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import MyButton from "@/app-example/components/MyButton";
+import { useRouter } from 'expo-router';
 
 const Signup = () => {
+  const router = useRouter();
+
+  const onRegister = () => {
+    router.navigate("/login");
+  };
+
   return (
-    <View>
-      <Text>SignUp</Text>
-    </View>
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <MyButton title={"Register"} onPress={onRegister} />
+      </View>
   );
 };
 
