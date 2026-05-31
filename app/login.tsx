59d206c59241f5ca531756363beb86871988913f
Atul Anand <atulanand092004@gmail.com>
2026-05-31 16:09:14 +0530

    Make-Login-SignUp Pages

--- app/login.tsx
+++ app/login.tsx
@@ -1,7 +1,8 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image } from 'react-native'
 import React from 'react'
 import MyButton from "@/app-example/components/MyButton";
 import { useRouter } from 'expo-router';
+import { TextInput } from 'react-native';
 
 const Login = () => {
     const router = useRouter();
@@ -9,8 +10,38 @@ const Login = () => {
         router.navigate("/signup");
     };
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+    <View style={{ flex: 1}}>
+    <Image source={require("@/assets/images/login.jpg")} style={{
+        width: "100%",
+        height: "70%",
+        backgroundColor: "lightgray",
+    }}
+    resizeMode="cover"
+    />
+    <View style={{ padding: 20 , gap: 10}}>
+    <TextInput 
+    placeholder="Enter your email" 
+    style={{ 
+        borderWidth: 1, 
+        borderColor: "red", 
+        width: "100%",
+         padding: 10, 
+         marginBottom: 10,
+         borderRadius: 10 
+         }}
+         onChangeText={(e) => console.log(e)}>   # This is how to get the storevalue form the text input.
+         </TextInput>
+    <TextInput 
+    placeholder="Enter your password" style={{ 
+        borderWidth: 1, 
+        borderColor: "red", 
+        width: "100%",
+         padding: 10, 
+         marginBottom: 10,
+         borderRadius: 10 }}>
+         </TextInput>
       <MyButton title={"Login"} onPress={onLogin} />
+      </View>
       </View>
   );
 };
--- app/signup.tsx
+++ app/signup.tsx
@@ -1,7 +1,8 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image } from 'react-native'
 import React from 'react'
 import MyButton from "@/app-example/components/MyButton";
 import { useRouter } from 'expo-router';
+import { TextInput } from 'react-native';
 
 const Signup = () => {
   const router = useRouter();
@@ -11,9 +12,37 @@ const Signup = () => {
   };
 
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <MyButton title={"Register"} onPress={onRegister} />
-      </View>
+    <View style={{ flex: 1 }}>
+      <Image
+        source={require("@/assets/images/register.jpg")}
+        style={{
+          width: "100%",
+          height: "80%",
+        }} 
+      />
+      <TextInput 
+          placeholder="Enter your email" 
+          style={{ 
+              borderWidth: 1, 
+              borderColor: "red", 
+              width: "100%",
+               padding: 10, 
+               marginBottom: 10,
+               borderRadius: 20 
+               }}
+               onChangeText={(e) => console.log(e)}>   
+               </TextInput>
+          <TextInput 
+          placeholder="Enter your password" style={{ 
+              borderWidth: 1, 
+              borderColor: "red", 
+              width: "100%",
+               padding: 10, 
+               marginBottom: 10,
+               borderRadius: 20 }}>
+               </TextInput>
+      <MyButton title="Register" onPress={onRegister} />
+    </View>
   );
 };
 
